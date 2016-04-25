37f48352915ca02eebe1aad557247f3d84c8d300
yyfjmb <yyfjmb@hotmail.com>
2016-04-25 14:33:30 +0800

    update data

---
+++ articles.js
@@ -0,0 +1,80 @@
+{"articles": [
+    {"date1": "APR", "date2": "20", "author": "", 
+        "title":"袁瑋熙：如何用選票打敗獨裁者？", 
+        "text": "学者认为，真正导致民主转型，取决于一个近乎众所周知的原因：反对阵营能否共同进退。",
+        "link": "https://theinitium.com/article/20160420-opinion-samsonyuen-legcoelection/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/image/2016/04/901ffa35d2a7492c95ed3b600a538536.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "APR", "date2": "13", "author": "許創彥", 
+        "title": "是時候，走進選舉前的香港立法會", 
+        "text": "立法會大堂掛着藝術裝置《人物》，象徵這裏屬於公眾。今天，我走進立法會，只見到五個保安、兩個職員，還有一部安檢機⋯⋯",
+        "link": "https://theinitium.com/article/20160413-hongkong-legco-tour/", 
+        "image": "images/headline_image2.jpg"
+    },
+    {   "date1": "APR", "date2": "11", "author": "涂青華 趙燕婷", 
+        "title": "黎汶洛：參選立法會升級「打大佬」", 
+        "text": "21歲的黎汶洛以「打機」形容參選立法會：「爭取民主就像『打大佬』，要贏先要儲Level。」",
+        "link": "https://theinitium.com/article/20160411-hongkong-legco-oscar/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/image/2016/04/3a342733246f4d5bbb24cce6e76bf918.jpg"
+    },
+    {	"date1": "APR", "date2": "7", "author": "趙燕婷", 
+    	"title": "侯志強又來了:「我要組黨創世界。」", 
+    	"text": "鄉事派侯志強組黨出戰立法會選舉，習慣發號施令的他如何適應議事文化﹖他說：「我要做立法會主席。」",
+    	"link": "https://theinitium.com/article/20160407-hongkong-legcohau/", 
+    	"image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/image/2016/03/03714b27024e41b1ab68ace70e8b86ab.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+	{	"date1": "MAR", "date2": "21", "author": "趙燕婷", 
+	    "title": "專訪：突然辭任的李慧琼，她有備而來嗎？", 
+	    "text": "李慧琼突然辭任行政會議成員，外界猜測她部署參選特首，她的回應是——「不要讓市民覺得民建聯等於政府」。", 
+	    "link": "https://theinitium.com/article/20160321-hongkong-legcostarry/", 
+	    "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/image/2016/03/a6fdf4d7f98547949bbed4c9392a2fbd.jpg"
+	},
+    {	"date1": "MAR", "date2": "3", "author": "", 
+    	"title": "馬嶽：周浩鼎沒有拿到的票", 
+    	"text": "如果我們相信228的得票數字，是「建制派不團結」，未能真正反映他們全部實力的話，則他們四年來組織票已經有相當增長，以及泛民可能真的已經流失了小量中間票源。", 
+    	"link": "https://theinitium.com/article/20160303-opinion-mangok-legcoelection/", 
+    	"image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/ee3a8f4e0b6a46b3962e1b4436c0cef9.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "MAR", "date2": "2", "author": "", 
+        "title": "無妄齋：「三分天下」──反對派光譜的變局", 
+        "text": "過去數年一直建立論述、累積行動事跡的本土派迅速崛起，正式挑戰泛民地位，可以預見後者支持度將進一步下滑，反建制力量將重新訂定政治光譜。箇中因由，值得深思。", 
+        "link": "https://theinitium.com/article/20160302-opinion-legcoelection2/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/506d311d10a24fd5b3ac6acc8ac16c99.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "MAR", "date2": "1", "author": "陳倩兒 黃銘浩", 
+        "title": "從「暴動」到選舉，本土新生代梁天琦說：我不想失敗，我想贏", 
+        "text": "24歲的港大生梁天琦在香港政治舞台橫空出世，他公開表示相信暴力，區隔異族。激進背後，他走過怎樣的成長路？新移民母親和做歷史教師的父親又怎樣影響了他？", 
+        "link": "https://theinitium.com/article/20160302-opinion-legcoelection2/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/eb2be1f4374e4d12a5961dd303f6c18e.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "FEB", "date2": "29", "author": "趙燕婷 陳嘉茵 黃銘浩", 
+        "title": "新界東立法會補選結果，標誌香港正式進入泛民、建制、本土三足鼎立局面", 
+        "text": "補選結果清晨公布，屬傳統泛民主派的公民黨楊岳橋勝出。而首次打正旗號出選的本土派，參選人梁天琦亦獲15%高票，定下本土派的基本盤。", 
+        "link": "https://theinitium.com/article/20160229-hongkong-legcoelectionresult/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/36e1cea22db7441c967cacab819d4131.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "FEB", "date2": "23", "author": "趙燕婷", 
+        "title": "當泛民主派說「守住關鍵一席」，他們卻沒踩準「關鍵」？", 
+        "text": "旺角騷亂後，本土派候選人名聲大振，泛民選情徒增變數，228補選結果即將出爐，除了一個議席的歸屬，我們還能從票箱中窺見更多。", 
+        "link": "https://theinitium.com/article/20160223-hongkong-legcoelection201602/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/09a3978bb1a24fe6b46f18f130f65b06.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "FEB", "date2": "22", "author": "趙燕婷 黃銘浩", 
+        "title": "旺角騷亂後，228新界東選民的最後掙扎：投票給誰？", 
+        "text": "一邊是政治表態，另一邊是守住關鍵一席。星期日的立法會補選，原本是泛民主派與建制派的對局，在旺角騷亂後增添變數，這變數也是未來香港政治的新形態。", 
+        "link": "https://theinitium.com/article/20160222-hongkong-legcoelection201601/", 
+        "image": "images/headline_image1.jpg"
+    },
+    {   "date1": "JAN", "date2": "2", "author": "", 
+        "title": "鍾耀華：社運四派割裂 立會選舉直面前途問題", 
+        "text": "傘運後的政治動員乏力只是香港政治生態的表徵，在這之下是中港憲政關係的想像與不安，甚至是香港2047年的前途爭辯。", 
+        "link": "https://theinitium.com/article/20160102-opinion-easonchung-2047/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/5823ceb736744bcab743c7f3e5e75ea4.jpg?imageView2/1/w/240/h/240/format/jpg"
+    },
+    {   "date1": "AUG", "date2": "15", "author": "端Lab製作", 
+        "title":"20萬條投票紀錄帶你解碼香港立法會", 
+        "text": "泛民建制真的水火不容？最受泛民支持的建制派是誰？204470個立法會投票紀錄的熱度圖為你解開立法會之謎。", 
+        "link": "https://theinitium.com/article/20150812-hongkong-legcoanalysis/", 
+        "image": "https://d3rhr7kgmtrq1v.cloudfront.net/media/9d2270f49e6245a2839274a7ebf61c4f.jpg?imageView2/1/w/240/h/240/format/jpg"
+    }
+]}
